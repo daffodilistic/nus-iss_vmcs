310b4e12360c93bdb801428003defb3bb186a5d1
Soh Thiam Hing <daffodilistic@gmail.com>
2021-11-17 01:39:45 +0800

    Fix bug where only one drink object is always being returned

--- backend/src/main/java/com/cooldrinkscompany/vmcs/VendingMachineService.java
+++ backend/src/main/java/com/cooldrinkscompany/vmcs/VendingMachineService.java
@@ -1,23 +1,25 @@
 package com.cooldrinkscompany.vmcs;
 
+import java.util.Collections;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonBuilderFactory;
 import javax.json.JsonObject;
 
-import com.cooldrinkscompany.vmcs.pojo.Drink;
-
-import java.util.logging.Level;
-
-import io.helidon.dbclient.DbClient;
 import io.helidon.common.reactive.Multi;
+import io.helidon.dbclient.DbClient;
+import io.helidon.webserver.Routing;
 import io.helidon.webserver.ServerRequest;
 import io.helidon.webserver.ServerResponse;
 import io.helidon.webserver.Service;
-import io.helidon.webserver.Routing;
-
 
 public class VendingMachineService implements Service {
     private static final Logger LOGGER = Logger.getLogger(VendingMachineService.class.getName());
+    private static final JsonBuilderFactory JSON_FACTORY = Json.createBuilderFactory(Collections.emptyMap());
 
     private final DbClient dbClient;
 
@@ -25,10 +27,9 @@ public class VendingMachineService implements Service {
         this.dbClient = dbClient;
 
         // MySQL init
-        dbClient.execute(handle -> handle.createInsert(
-            "CREATE TABLE public.drinks (id SERIAL PRIMARY KEY, name VARCHAR NULL)")
-            .execute()).thenAccept(System.out::println)
-                .exceptionally(throwable -> {
+        dbClient.execute(handle -> handle
+                .createInsert("CREATE TABLE public.drinks (id SERIAL PRIMARY KEY, name VARCHAR NULL)").execute())
+                .thenAccept(System.out::println).exceptionally(throwable -> {
                     LOGGER.log(Level.WARNING, "Failed to create table, maybe it already exists?", throwable);
                     return null;
                 });
@@ -40,10 +41,25 @@ public class VendingMachineService implements Service {
     }
 
     private void listDrinks(ServerRequest request, ServerResponse response) {
-        // Multi<JsonObject> rows = dbClient.execute(exec -> exec.createQuery("SELECT * FROM drinks").execute())
+        // Multi<JsonObject> rows = dbClient.execute(exec -> exec.createQuery("SELECT *
+        // FROM drinks").execute())
         Multi<JsonObject> rows = dbClient.execute(exec -> exec.namedQuery("select-all"))
                 .map(it -> it.as(JsonObject.class));
 
-        response.send(rows, JsonObject.class);
+        JsonArrayBuilder jsonBuilder = JSON_FACTORY.createArrayBuilder();
+
+        rows.forEach(it -> {
+            System.out.println(it.toString());
+            jsonBuilder.add(it);
+        }).whenComplete((input, exception) -> {
+            if (exception != null) {
+                LOGGER.log(Level.WARNING, "Failed to list drinks", exception);
+                response.status(500).send();
+                return;
+            }
+
+            JsonArray jsonArray = jsonBuilder.build();
+            response.send(jsonArray);
+        });
     }
 }
