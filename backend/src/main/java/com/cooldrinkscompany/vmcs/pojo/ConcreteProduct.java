b005425f87f8d3b03f2216b3ea712e28d0d04055
Soh Thiam Hing <daffodilistic@gmail.com>
2021-11-25 11:16:04 +0800

    Convert C# code for ConcreteProduct.java
    This file might not be used/necessary?

--- backend/src/main/java/com/cooldrinkscompany/vmcs/pojo/ConcreteProduct.java
+++ backend/src/main/java/com/cooldrinkscompany/vmcs/pojo/ConcreteProduct.java
@@ -1,16 +1,15 @@
 package com.cooldrinkscompany.vmcs.pojo;
-{  
-    class VendingDrink : Drink  
+
+    class VendingDrink extends Product  
     {  
         private int _id;
         private String _name;
         private int _quantity;
         private double _price = 0.0d;
   
-        public VendingDrink(int id, string name)  
+        public VendingDrink(int id, String name)  
         {  
             _id = id;
             _name = name;
         }
     }  
-}  
