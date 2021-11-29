169f723d73aface4e7f209943ef5fb7338d6d704
mnyue <liumigyue@gmail.com>
2021-11-29 10:47:52 +0800

    Add door status validation on set/vew CoinQty

--- backend/src/main/java/com/cooldrinkscompany/vmcs/service/CoinsService.java
+++ backend/src/main/java/com/cooldrinkscompany/vmcs/service/CoinsService.java
@@ -101,14 +101,20 @@ public class CoinsService implements Service {
 
     private void viewCoinQty(ServerRequest request, ServerResponse response) {
         LOGGER.info("start viewing coins qty");
+        if(ControllerSetSystemStatus.getStatus(this.productDao,"isUnlocked")){
         String coinName = request.path().toString().replace("/viewCoinQty/", "");
         int qty = ControllerManageCoin.queryCoinQty(this.productDao, coinName);
         JsonObject returnObject = JSON_FACTORY.createObjectBuilder().add("Coin Qty:", qty !=Integer.MAX_VALUE ? String.valueOf(qty) : "ERROR: Coin type does not exist").build();
         response.send(returnObject);
+        }else{
+        JsonObject returnObject = JSON_FACTORY.createObjectBuilder().add("Set Coin Qty:", "Door locked. Please unlock door first.").build();
+        response.send(returnObject);
+        }
     }
 
     private void setCoinQty(ServerRequest request, ServerResponse response) {
         LOGGER.info("start setting coins qty");
+        if(ControllerSetSystemStatus.getStatus(this.productDao,"isUnlocked")){
         String coinParams = request.path().toString().replace("/setCoinQty/", "");
         String[] coinTypeAndQty = coinParams.split(":");
         if (coinTypeAndQty.length != 2) {
@@ -121,6 +127,10 @@ public class CoinsService implements Service {
             String status = ControllerManageCoin.setCoinQty(this.productDao, coinType, coinQty);
             JsonObject returnObject = JSON_FACTORY.createObjectBuilder().add("Status:", status).build();
             response.send(returnObject);
+        }
+        }else{
+        JsonObject returnObject = JSON_FACTORY.createObjectBuilder().add("Set Coin Qty:", "Door locked. Please unlock door first.").build();
+        response.send(returnObject);
         }
     }
 
